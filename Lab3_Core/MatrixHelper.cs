b4fce49c4c4a489ac0375ae9fe440357817fcf0c
Evgeni <eugenluzin@gmail.com>
2022-04-17 22:33:04 +0500

    Добавьте файлы проекта.

---
+++ Lab3_Core/MatrixHelper.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Lab3_Core
+{
+    internal class MatrixHelper
+    {
+                          
+        static public int[,] FillingMatrix(int height, int width) 
+        {
+        int[,] Matrix = new int[height, width];
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Matrix[i, j] = 3 * i + j;
+                }
+            }
+            return Matrix;
+        }
+        static public void Print(int[,] Matrix)
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                Console.Write(" ");
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{Matrix[i, j],-5}| ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static public int[,] MultiplyMatrix_Standart(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] MultiplyMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            for (int i = 0; i < MultiplyMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < MultiplyMatrix.GetLength(1); j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+
+                        sum += matrix1[i, k] * matrix2[k, j];
+                    }
+                    MultiplyMatrix[i, j] = sum;
+                }
+            }
+
+            return MultiplyMatrix;
+        }
+        static public int[,] MultiplyMatrix_1(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] MultiplyMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            Parallel.For(0, MultiplyMatrix.GetLength(0), i =>
+             {
+                 for (int j = 0; j < MultiplyMatrix.GetLength(1); j++)
+                 {
+                     int sum = 0;
+                     for (int k = 0; k < matrix1.GetLength(1); k++)
+                     {
+
+                         sum += matrix1[i, k] * matrix2[k, j];
+                     }
+                     MultiplyMatrix[i, j] = sum;
+                 }
+             });
+
+            return MultiplyMatrix;
+        }
+       
+        static public int[,] MultiplyMatrix_2(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] MultiplyMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            Parallel.For(0, MultiplyMatrix.GetLength(0), i =>
+            {
+                Parallel.For(0, MultiplyMatrix.GetLength(1), j =>
+                {
+                    int sum = 0;
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+
+                        sum += matrix1[i, k] * matrix2[k, j];
+                    }
+                    MultiplyMatrix[i, j] = sum;
+                });
+            });
+
+            return MultiplyMatrix;
+        }
+        static public int[,] MultiplyMatrix_3(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] MultiplyMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            Parallel.For(0, MultiplyMatrix.GetLength(0), i =>
+            {
+                Parallel.For(0, MultiplyMatrix.GetLength(1), j =>
+                {
+                int sum = 0;
+
+                Parallel.For(0, matrix1.GetLength(1), k =>
+                {
+                        sum += matrix1[i, k] * matrix2[k, j];
+                });
+                    MultiplyMatrix[i, j] = sum;
+                });
+            });
+
+            return MultiplyMatrix;
+        }
+        static public int[,] MultiplyMatrix_Only2(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] MultiplyMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            for (int i = 0; i < MultiplyMatrix.GetLength(0); i++)
+            {
+                Parallel.For(0, MultiplyMatrix.GetLength(1), j =>
+                {
+                    int sum = 0;
+
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+
+                        sum += matrix1[i, k] * matrix2[k, j];
+                    }
+
+                    MultiplyMatrix[i, j] = sum;
+                });
+            }
+
+            return MultiplyMatrix;
+        }
+    }
+}
---
+++ Lab3_Core/Program.cs
@@ -0,0 +1,62 @@
+﻿using Lab3_Core;
+using System;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = MatrixHelper.FillingMatrix(5,3);
+            int[,] matrix2 = MatrixHelper.FillingMatrix(3,4);
+            if (matrix1.GetLength(1) == matrix2.GetLength(0))
+            {
+                int[,] MultiplyMatrix = MatrixHelper.MultiplyMatrix_Standart(matrix1, matrix2);
+                int[,] MultiplyMatrix1 = MatrixHelper.MultiplyMatrix_1(matrix1, matrix2);
+                int[,] MultiplyMatrix2 = MatrixHelper.MultiplyMatrix_2(matrix1, matrix2);
+                int[,] MultiplyMatrix3 = MatrixHelper.MultiplyMatrix_3(matrix1, matrix2);
+                int[,] MultiplyMatrix_Only2 = MatrixHelper.MultiplyMatrix_Only2(matrix1, matrix2);
+
+                Console.WriteLine("1 матрица");
+                MatrixHelper.Print(matrix1);
+                Console.WriteLine("2 матрица");
+                MatrixHelper.Print(matrix2);
+                Console.WriteLine("Результат умножения двух матриц (Последовательно)");
+                MatrixHelper.Print(MultiplyMatrix);
+                Console.WriteLine("Результат умножения двух матриц (Параллельно только для каждой строки)");
+                MatrixHelper.Print(MultiplyMatrix1);
+                Console.WriteLine("Результат умножения двух матриц (Параллельно для строки и элемента)");
+                MatrixHelper.Print(MultiplyMatrix2);
+                Console.WriteLine("Результат умножения двух матриц (Параллельно для строки, элемента и нахождения суммы для элемента)");
+                MatrixHelper.Print(MultiplyMatrix3);
+                Console.WriteLine("Результат умножения двух матриц (Параллельно только для каждого элемента)");
+                MatrixHelper.Print(MultiplyMatrix_Only2);
+            }
+            else
+                Console.WriteLine("Неверный размер матриц");
+            
+
+
+            /*for (int i = 0; i < newMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < newMatrix.GetLength(1); j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < matrix1.GetLength(1); k++)
+                    {
+                       
+                        sum += matrix1[i, k] * matrix2[k, j];
+                    }
+                    newMatrix[i, j] = sum;
+                }
+            }
+            */
+
+
+
+        }
+    }
+    
+}
+
